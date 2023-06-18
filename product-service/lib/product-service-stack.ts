fd6f12038e34d40a6dd6b8a56a289867a10e93b0
Mark Karnaukh <marik13021992@gmail.com>
2023-06-18 16:22:37 +0200

    Adjust basic project structure

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -7,14 +7,14 @@ import { SwaggerUi } from '@pepperize/cdk-apigateway-swagger-ui';
 import * as path from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class NodejsAwsShopBackendStack extends cdk.Stack {
+export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'NodejsAwsShopBackendQueue', {
+    // const queue = new sqs.Queue(this, 'ProductServiceStack', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
@@ -52,8 +52,8 @@ export class NodejsAwsShopBackendStack extends cdk.Stack {
       },
     });
 
-    const api = new apigateway.RestApi(this, 'BackendShopAPI', {
-      description: 'Aws NodeJs Backend Shop API',
+    const api = new apigateway.RestApi(this, 'ProductServiceAPI', {
+      description: 'Product Service REST API',
       deployOptions: {
         stageName: 'dev', // Deployment stages: 'dev' or 'prod'. By default, the stageName is set to prod.
       },
