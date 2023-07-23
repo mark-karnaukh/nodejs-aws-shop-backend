080f43df267cfce4bb5158edd73705da7c58fa39
Mark Karnaukh <marik13021992@gmail.com>
2023-07-23 22:01:01 +0200

    Enable CORS

--- bff-service/index.js
+++ bff-service/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(
   '/product-api',
