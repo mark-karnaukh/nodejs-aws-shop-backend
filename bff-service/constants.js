0b9d93fdfabfc8eb3bed0b46ffb1ed3fc635e955
Mark Karnaukh <marik13021992@gmail.com>
2023-07-23 22:12:54 +0200

    Add constants and rewrite files to use ES6 imports

---
+++ bff-service/constants.js
@@ -0,0 +1,2 @@
+export const CART_API_SERVICE_KEY = 'cart-api';
+export const PRODUCT_API_SERVICE_KEY = 'product-api';
--- bff-service/index.js
+++ bff-service/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const proxy = require('http-proxy-middleware');
-const cors = require('cors');
+import { CART_API_SERVICE_KEY, PRODUCT_API_SERVICE_KEY } from './constants.js';
+
+import express from 'express';
+import proxy from 'http-proxy-middleware';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
@@ -8,24 +10,24 @@ const port = 3000;
 app.use(cors());
 
 app.use(
-  '/product-api',
+  `/${PRODUCT_API_SERVICE_KEY}`,
   proxy.createProxyMiddleware({
     target: 'https://wfvhgvxizg.execute-api.eu-central-1.amazonaws.com/dev',
     changeOrigin: true,
     pathRewrite: {
-      '^/product-api': '',
+      [`^/${PRODUCT_API_SERVICE_KEY}`]: '',
     },
   })
 );
 
 app.use(
-  '/cart-api',
+  `/${CART_API_SERVICE_KEY}`,
   proxy.createProxyMiddleware({
     target:
       'http://mark-karnaukh-cart-api-dev.eu-central-1.elasticbeanstalk.com/api',
     changeOrigin: true,
     pathRewrite: {
-      '^/cart-api': '',
+      [`^/${CART_API_SERVICE_KEY}`]: '',
     },
   })
 );
