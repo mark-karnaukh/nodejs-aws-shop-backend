cc48bd968a3b285b6f69b13c041af67bab615d3f
Mark Karnaukh <marik13021992@gmail.com>
2023-07-23 21:40:12 +0200

    Initialize express.js app + implement initial logic for request handling

---
+++ bff-service/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+
+const app = express();
+const port = 3000;
+
+app.use(
+  '/product-api',
+  proxy.createProxyMiddleware({
+    target: 'https://wfvhgvxizg.execute-api.eu-central-1.amazonaws.com/dev',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/product-api': '',
+    },
+  })
+);
+
+app.use(
+  '/cart-api',
+  proxy.createProxyMiddleware({
+    target:
+      'http://mark-karnaukh-cart-api-dev.eu-central-1.elasticbeanstalk.com/api',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/cart-api': '',
+    },
+  })
+);
+
+app.all('/*', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
